e404b48904b7ead8917ee81a179c6374dceede59
David DeBoer <ddeboer@berkeley.edu>
2017-11-22 11:23:57 -0800

    fixed formating in use

--- use.py
+++ use.py
@@ -5,7 +5,7 @@ import argparse
 import prog_path
 
 overallPath = prog_path.pyPlanetPath
-constitPath = prop_path.constituentPath
+constitPath = prog_path.constituentPath
 constituents = os.listdir(constitPath)
 
 o = argparse.ArgumentParser(prefix_chars='-+')
@@ -26,18 +26,18 @@ for c in constituents:
             s = fp.readline().strip()
             fp.close()
             if argdict[c]:
-                print('--- {:-8s} Toggle to not use {}'.format(c, s))
+                print('--- {:8s} Toggle to not use {}'.format(c, s))
                 os.rename(os.path.join(cpth, 'use.txt'), os.path.join(cpth, 'nouse.txt'))
             else:
-                print('+++ {-8s} Using {}'.format(c, s))
+                print('+++ {:8s} Using {}'.format(c, s))
         elif 'nouse.txt' in fil:
             fp = open(os.path.join(cpth, 'nouse.txt'), 'r')
             s = fp.readline().strip()
             fp.close()
             if argdict[c]:
-                print('+++ {:-8s} Toggle to use {}'.format(c, s))
+                print('+++ {:8s} Toggle to use {}'.format(c, s))
                 os.rename(os.path.join(cpth, 'nouse.txt'), os.path.join(cpth, 'use.txt'))
             else:
-                print('--- {:-8s} Not using {}'.format(c, s))
+                print('--- {:8s} Not using {}'.format(c, s))
         else:
-            print('    {:-8s} Use/nouse.txt not found'.format(c))
+            print('    {:8s} Use/nouse.txt not found'.format(c))
