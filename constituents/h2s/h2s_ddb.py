019af0dc15d227840776fd5a473fbf36f4e4ccd7
David DeBoer <ddeboer@berkeley.edu>
2017-11-21 23:23:49 -0800

    lots of constituents updats

--- constituents/h2s/h2s_ddb.py
+++ constituents/h2s/h2s_ddb.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
-import math
 import os.path
+import numpy as np
 
 # Some constants
 coef = 7.244E+21     # coefficient from GEISA_PP.TEX eq. 14
@@ -8,49 +8,31 @@ T0 = 296.0           # reference temperature in K
 hck = 1.438396       # hc/k  [K cm]
 GHz = 29.9792458     # conversion from cm^-1 to GHz
 
-# Set data arrays
-f0 = []
-I0 = []
-E = []
-GH2S = []
+data = None
 
 
-def readInputFiles(path, verbose=False):
-    """If needed this reads in the data files for h2s"""
-    useLinesUpTo = 200   # index number
-    global nlin
-    nlin = 0
+def readInputFiles(path, verbose):
+    filename = os.path.join(path, 'h2s.npz')
     if verbose:
-        print("Reading h2s lines")
-    filename = os.path.join(path, 'h2s.lin')
-    ifp = open(filename, 'r')
-    for line in ifp:
-        if nlin >= useLinesUpTo:
-            break
-        nlin += 1
-        data = line.split()
-        if len(data) == 4:
-            f0.append(float(data[0]))
-            I0.append(float(data[1]))
-            E.append(float(data[2]))
-            GH2S.append(float(data[3]))
-        else:
-            break
-    ifp.close()
-    if verbose:
-        print('   ' + str(nlin) + ' lines')
-    return nlin
+        print("Reading h2s lines from {}".format(filename))
+    global data
+    data = np.load(filename)
 
 
 def alpha(freq, T, P, X, P_dict, otherPar, units='dBperkm', path='./', verbose=False):
+    """Computes absorption due to h2s"""
 
     # Read in data if needed
-    if len(f0) == 0:
+    if data is None:
         readInputFiles(path, verbose)
 
     P_h2 = P * X[P_dict['H2']]
     P_he = P * X[P_dict['HE']]
     P_h2s = P * X[P_dict['H2S']]
+    f0 = data['f0']
+    I0 = data['I0']
+    E = data['E']
+    GH2S = data['GH2S']
     GH2 = 1.960
     GHe = 1.200
     ZH2 = 0.000
@@ -61,25 +43,22 @@ def alpha(freq, T, P, X, P_dict, otherPar, units='dBperkm', path='./', verbose=F
     n_dvl = 0.7
     n_int = 3.0 / 2.0
     delta = D * P_h2s
+    gamma = np.power((T0 / T), n_dvl) * (GH2 * P_h2 + GHe * P_he + GH2S * P_h2s)
+    g2 = gamma**2
+    zeta = gamma
+    z2 = zeta**2
+    ITG = I0 * np.exp(-((1.0 / T) - (1.0 / T0)) * E * hck)
 
     alpha_h2s = []
     for f in freq:
         f2 = f**2
-        alpha = 0.0
-        for i in range(nlin):
-            gamma = pow((T0 / T), n_dvl) * (GH2 * P_h2 + GHe * P_he + GH2S[i] * P_h2s)
-            g2 = gamma**2
-            zeta = gamma
-            z2 = zeta**2
-            ITG = I0[i] * math.exp(-((1.0 / T) - (1.0 / T0)) * E[i] * hck)
-            num = (gamma - zeta) * f2 + (gamma + zeta) * (pow(f0[i] + delta, 2.0) + g2 - z2)
-            den = pow((f2 - pow(f0[i] + delta, 2.0) - g2 + z2), 2.0) + 4.0 * f2 * g2
-            shape = GHz * 2.0 * pow(f / f0[i], 2.0) * num / (math.pi * den)
-            alpha += shape * ITG
+        num = (gamma - zeta) * f2 + (gamma + zeta) * (np.power(f0 + delta, 2.0) + g2 - z2)
+        den = np.power((f2 - np.power(f0 + delta, 2.0) - g2 + z2), 2.0) + 4.0 * f2 * g2
+        shape = GHz * 2.0 * np.power(f / f0, 2.0) * num / (np.pi * den)
+        alpha_h2s.append(np.sum(shape * ITG))
 
-        a = coef * (P_h2s / T0) * pow((T0 / T), n_int + 2) * alpha
-        if units == 'dBperkm':
-            a *= 434294.5
-        alpha_h2s.append(a)
+    alpha_h2s = coef * (P_h2s / T0) * pow((T0 / T), n_int + 2) * np.array(alpha_h2s)
+    if units == 'dBperkm':
+        alpha_h2s *= 434294.5
 
     return alpha_h2s
--- constituents/ph3/ph3_jh.py
+++ constituents/ph3/ph3_jh.py
@@ -1,109 +1,72 @@
-import math
+from __future__ import absolute_import, division, print_function
 import os.path
+import numpy as np
 
 # Some constants
 coef = 7.244E+21        # coefficient from GEISA_PP.TEX eq. 14
 T0 = 300.0              # reference temperature in K
 hck = 1.438396          # hc/k  [K cm]
 GHz = 29.9792458        # conversion from cm^-1 to GHz
-PI = math.pi
+PI = np.pi
 
 # Set data arrays and values
-f0 = []
-I0 = []
-E = []
-WgtI0 = []
-WgtFGB = []
-WgtSB = []
-def readInputFiles(path,verbose=False):
-    """If needed this reads in the data files for ph3"""
+data = None
+data_wgt = {}
 
-    # Read line info
-    useLinesUpTo = 320
-    filename = os.path.join(path,'ph3jh.lin')
-    if verbose:
-        print "Reading ph3 data "+filename
-    ifp = open(filename,'r')
-    global nlin
-    nlin = 0
-    for line in ifp:
-        if nlin >= useLinesUpTo:
-            break
-        nlin+=1
-        data = line.split()
-        if len(data) == 3:
-            f0.append(float(data[0]))
-            I0.append(float(data[1])/(GHz*1.0E17))
-            E.append(float(data[2]))
-        else:
-            break
-    ifp.close()
-    if verbose:
-        print '   '+str(nlin)+' lines'
 
-    # Read weight info
-    filename = os.path.join(path,'PH3WGT.dat')
-    ifp = open(filename,'r')
-    numWgt = 40
-    global nwgt
-    nwgt = 0
-    for line in ifp:
-        if nwgt >= numWgt:
-            break
-        nwgt+=1
-        data = line.split()
-        if len(data) == 4:
-            WgtI0.append(float(data[1]))
-            WgtFGB.append(float(data[2]))
-            WgtSB.append(float(data[3]))
-        else:
-            break
-    ifp.close()
+def readInputFiles(path, verbose=False):
+    filename = os.path.join(path, 'ph3jh.npz')
     if verbose:
-        print '    '+str(nwgt)+' weights'
-    return nlin
+        print("Reading ph3 lines from  {}".format(filename))
+    global data
+    data = np.load(filename)
+    filename = os.path.join(path, 'PH3WGT.npz')
+    if verbose:
+        print("Reading ph3 wgts from {}".format(filename))
+    global data_wgt
+    data_wgt = np.load(filename)
 
-def alpha(freq,T,P,X,P_dict,otherPar,units='dBperkm',path='./',verbose=False):
+
+def alpha(freq, T, P, X, P_dict, otherPar, units='dBperkm', path='./', verbose=False):
     """Computes the absorption due to ph3"""
 
     # Read in data if needed
-    if len(f0)==0:
-        readInputFiles(path,verbose=verbose)
+    if data is None:
+        readInputFiles(path, verbose=verbose)
 
-    P_h2 = P*X[P_dict['H2']]
-    P_he = P*X[P_dict['HE']]
-    P_ph3= P*X[P_dict['PH3']]
-    
+    P_h2 = P * X[P_dict['H2']]
+    P_he = P * X[P_dict['HE']]
+    P_ph3 = P * X[P_dict['PH3']]
+    f0 = data['f0']
+    I0 = data['I0']
+    E = data['E']
+    WgtI0 = data_wgt['WgtI0']
+    WgtFGB = data_wgt['WgtFGB']
+    WgtSB = data_wgt['WgtSB']
+
     # Line parameters
-    GH2   = 3.2930
-    GHe   = 1.6803
-    GPH3  = 4.2157
-    n_dvl = 2.0/3.0
-    n_int = 3.0/2.0
-    zeta  = 0.0
+    GH2 = 3.2930
+    GHe = 1.6803
+    GPH3 = 4.2157
+    n_dvl = 2.0 / 3.0
+    n_int = 3.0 / 2.0
+    zeta = 0.0
     z2 = zeta**2
     delta = 0.0
+    gamma = np.power((T0 / T), n_dvl) * (GH2 * P_h2 + GHe * P_he) * WgtFGB + np.power((T0 / T), 1.0) * GPH3 * P_ph3 * WgtSB
+    g2 = gamma**2
+    ITG = I0 * WgtI0 * np.exp(-((1.0 / T) - (1.0 / T0)) * E * hck)
 
     alpha_ph3 = []
     for f in freq:
         f2 = f**2
-        alpha = 0.0
-        for i in range(nlin):
-            if i<nwgt:
-                gamma = pow((T0/T),n_dvl)*(GH2*P_h2 + GHe*P_he)*WgtFGB[i] + pow((T0/T),1.0)*GPH3*P_ph3*WgtSB[i]
-                g2 = gamma**2
-                ITG = I0[i]*WgtI0[i]*math.exp(-((1.0/T)-(1.0/T0))*E[i]*hck)
-            else:
-                gamma = pow((T0/T),n_dvl)*(GH2*P_h2 + GHe*P_he) + pow((T0/T),1.0)*GPH3*P_ph3
-                g2 = gamma**2
-                ITG = I0[i]*math.exp(-((1.0/T)-(1.0/T0))*E[i]*hck);
-            num = (gamma-zeta)*f2 + (gamma+zeta)*( (f0[i]+delta)**2 + g2 - z2)
-            den = (f2 - (f0[i]+delta)**2 - g2 + z2)**2 + 4.0*f2*g2
-            shape = GHz*2.0*((f/f0[i])**2)*num/(PI*den)
-            alpha += shape*ITG
-        a= coef*(P_ph3/T0)*pow((T0/T),n_int+2)*alpha
-        if units=='dBperkm':
-            a*=434294.5
-        alpha_ph3.append(a)
+        num = (gamma - zeta) * f2 + (gamma + zeta) * ((f0 + delta)**2 + g2 - z2)
+        den = (f2 - (f0 + delta)**2 - g2 + z2)**2 + 4.0 * f2 * g2
+        shape = GHz * 2.0 * ((f / f0)**2) * num / (PI * den)
+        alpha_ph3.append(np.sum(shape * ITG))
+
+    alpha_ph3 = coef * (P_ph3 / T0) * np.power((T0 / T), n_int + 2) * np.array(alpha_ph3)
+    if units == 'dBperkm':
+        alpha_ph3 *= 434294.5
 
     return alpha_ph3
